5642f2578ff6ecd10c9ef77c5da7d26c98a8b65f
Geoffrey Washburn <geoff@washburght.org>
2025-10-18 11:02:01 -0400

    Remove unpublished crates.

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,5 @@
 extern crate endgame_direction;
 extern crate endgame_egui;
-extern crate endgame_explorer;
-extern crate endgame_generation;
 extern crate endgame_grid;
 extern crate endgame_ludic;
 
